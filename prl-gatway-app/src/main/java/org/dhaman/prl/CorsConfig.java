29b235eefed83cf03aa87207f1157922ae684487
TarekBakr <tarek@dhaman.org>
2021-10-18 09:09:35 +0300

    gateway-app adding AuthorizationHeaderFilter and CorsConfig , securing routing

---
+++ prl-gatway-app/src/main/java/org/dhaman/prl/CorsConfig.java
@@ -0,0 +1,34 @@
+package org.dhaman.prl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public CorsWebFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();		
+		config.addAllowedOrigin("http://localhost");
+		config.addAllowedOrigin("http://localhost:8100");
+		config.addAllowedOrigin("capacitor://localhost");		
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		config.addExposedHeader("*");
+		config.applyPermitDefaultValues();
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsWebFilter(source);
+	}
+}
